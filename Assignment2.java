8f5fac520dd953d5e31f670a9cab18322f1da388
Dikshita <dikshitasingh826@gmail.com>
2024-03-12 19:43:15 +0530

    update on 12/02/24

--- Assignment2.java
+++ Assignment2.java
@@ -1,5 +1,122 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Assignment2 {
     public static void main(String[] args) {
-        
+        //Basic Programs
+        // Question1-7
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Enter the shape of which you want area :");
+//        String shape = input.next();
+//        double area;
+//        if(shape.equals("circle")){
+//            System.out.println("Enter Radius :");
+//            float r = input.nextInt();
+//            area = 3.14*r*r;
+//            System.out.println("Area of Circle is :"+area);
+//        }
+//        if(shape.equals("triangle")){
+//            System.out.println("Enter Height :");
+//            float h = input.nextInt();
+//            System.out.println("Enter Base :");
+//            float b = input.nextInt();
+//            area = (double) 1 /2*b*h;
+//            System.out.println("Area of Triangle is :"+area);
+//        }
+//        if(shape.equals("rectangle")){
+//            System.out.println("Enter Length :");
+//            float l = input.nextInt();
+//            System.out.println("Enter Breadth :");
+//            float br = input.nextInt();
+//            area = l*br;
+//            System.out.println("Area of Rectangle is :"+area);
+//        }
+//        if(shape.equals("rhombus")){
+//            System.out.println("Enter diagonal1 :");
+//            float d1 = input.nextInt();
+//            System.out.println("Enter diagonal2 :");
+//            float d2 = input.nextInt();
+//            area = (double) 1 /2*d1*d2;
+//            System.out.println("Area of Rhombus is :"+area);
+//        }
+//        if(shape.equals("equilateral triangle")){
+//            System.out.println("Enter oneside :");
+//            float a = input.nextInt();
+//            area = 0.8660*a*a;
+//            System.out.println("Area of Equilateral Triangle is :"+area);
+//        }
+//        else{
+//            System.out.println("Invalid shape");
+//        }
+        // Question 8-13
+//        Scanner input2 = new Scanner(System.in);
+//        System.out.println("Enter the shape of which you want Perimeter :");
+//        String shape = input2.next();
+//        double peri;
+//        if(shape.equals("circle")){
+//            System.out.println("Enter Radius :");
+//            float r = input2.nextInt();
+//            peri = 2*3.14*r;
+//            System.out.println("Perimeter of Circle is :"+peri);
+//        }
+//        if(shape.equals("equilateral triangle")){
+//            System.out.println("Enter side :");
+//            float s = input2.nextInt();
+//            peri = 3*s;
+//            System.out.println("Perimeter of Equilateral Triangle is :"+peri);
+//        }
+//        if(shape.equals("parallelogram")){
+//            System.out.println("Enter Height :");
+//            float h = input2.nextInt();
+//            System.out.println("Enter Breadth :");
+//            float b = input2.nextInt();
+//            peri = 2*(h*b);
+//            System.out.println("Perimeter of Parallelogram is :"+peri);
+//        }
+//        if(shape.equals("square")){
+//            System.out.println("Enter side :");
+//            float s = input2.nextInt();
+//            peri = 4*s;
+//            System.out.println("Perimeter of Square is :"+peri);
+//        }
+//        else{
+//            System.out.println("Invalid shape");
+//        }
+        // Question 14-18
+        //Volume od prism,cylinder,sphere,pyramid
+
+        // Question 24
+//        Scanner input3 = new Scanner(System.in);
+//        int sum = 0;
+//        int x=0;
+//        while (x >=0) {
+//            System.out.println("Enter a number :");
+//             x = input3.nextInt();
+//            if (x >0) {
+//                sum = sum + x;
+//                System.out.println(sum);
+//            }
+//            else {
+//                System.out.println("terminated");
+//                break;
+//            }
+//            System.out.println("Sum of entered Number is :"+sum);
+//        }
+
+        // Question 25
+//        Scanner input4 = new Scanner(System.in);
+//        int max =0;
+//        while(true){
+//            System.out.println("Enter the Number :");
+//            int n= input4.nextInt();
+//            if (n>max) {
+//               max = n;
+//            }
+//            if (n == 0  ) {
+//                break;
+//            }
+//            System.out.println("Maximum Number is :"+max);
+//        }
+
     }
 }
