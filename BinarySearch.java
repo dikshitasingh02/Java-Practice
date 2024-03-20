a6bb05789b80fb9b1a151f198d3097ffa3b88acf
Dikshita <dikshitasingh826@gmail.com>
2024-03-21 00:17:48 +0530

    update on 21/03/2024

---
+++ BinarySearch.java
@@ -0,0 +1,67 @@
+public class BinarySearch {
+    public static void main(String[] args) {
+        int[] arr = {-18, -12, -4, 0, 2, 3, 4, 15, 16, 18, 22, 45, 89};
+        int[] arr2 = {99, 80, 75, 22, 11, 10, 5, 2, -3};
+        int target = 22;
+        int ans = binarysearch(arr,target);
+        System.out.println(ans);
+        int ans2 = orderagnosticBS(arr2, target);
+        System.out.println(ans2);
+    }
+
+    //simple binary search
+    //return the index if does not exists return -1
+    static int binarysearch(int[] arr,int target){
+        int start =0;
+        int end = arr.length-1;
+
+        while(start<=end){
+            //find middle element
+         //   int mid = (start+end)/2;// might be possible that (start+end) exceds the range of int in java
+            int mid = start + (end-start)/2;
+
+            if(target<arr[mid]){
+                end=mid-1;
+            } else if (target>arr[mid]) {
+                start = mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+
+    //order agnostic binary search
+    static int orderagnosticBS(int[] arr,int target){
+        int start =0;
+        int end = arr.length-1;
+
+        //find wheather the array is sorted in ascending or descending
+        boolean isAsc = arr[start] < arr[end];
+
+        while(start<=end){
+            //find middle element
+            //   int mid = (start+end)/2;// might be possible that (start+end) exceds the range of int in java
+            int mid = start + (end-start)/2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+
+            if(isAsc){
+                if(target<arr[mid]){
+                    end=mid-1;
+                } else{
+                    start = mid+1;
+                }
+            }else{
+                if(target>arr[mid]){
+                    end=mid-1;
+                } else{
+                    start = mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+}
