1bc20b33ee7ec2dc950aef59c69b5bf4ac85105c
Dikshita <dikshitasingh826@gmail.com>
2024-04-04 23:44:39 +0530

    Update 04/04/2024

--- BubbleSort.java
+++ BubbleSort.java
@@ -2,46 +2,8 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr = {5,9,4,1,3,6};
-        insertion(arr);
+        bubble(arr);
         System.out.println(Arrays.toString(arr));
-    }
-
-    static void insertion(int[] arr){
-        for (int i = 0; i < arr.length-1; i++) {
-            for (int j = i+1; j > 0 ; j--) {
-                if(arr[j] < arr[j-1]){
-                    swap(arr,j,j-1);
-                }
-                else{
-                    break;
-                }
-            }
-        }
-    }
-
-    static void selection (int[] arr){
-        for (int i = 0; i < arr.length; i++) {
-            // find the max item in the remaining array and swap with correct index
-            int last = arr.length -i-1;
-            int maxIndex = getmaxindex(arr,0,last);
-            swap(arr,maxIndex,last);
-        }
-    }
-
-    static void swap(int[] arr,int first,int second){
-        int temp = arr[first];
-        arr[first] = arr[second];
-        arr[second] =  temp;
-    }
-
-    static int getmaxindex(int[] arr,int start,int end){
-        int max = start;
-        for (int i = start; i <= end ; i++) {
-            if(arr[max] < arr[i]){
-                max = i;
-            }
-        }
-        return max;
     }
 
     static void bubble(int[] arr){
